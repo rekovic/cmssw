889697b0e8308e995b992859b52ca7c7fc91b4a9
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-03-14 01:28:27 +0100

    remove additional pt cut for primary vertices with timing and remove redundant vertex collections

--- RecoVertex/Configuration/python/RecoVertex_cff.py
+++ RecoVertex/Configuration/python/RecoVertex_cff.py
@@ -16,21 +16,13 @@ from RecoJets.JetProducers.caloJetsForTrk_cff import *
 #timing
 from RecoVertex.PrimaryVertexProducer.TkClusParameters_cff import DA2DParameters
 unsortedOfflinePrimaryVertices1D = offlinePrimaryVertices.clone()
-unsortedOfflinePrimaryVertices1D.TkFilterParameters.minPt = cms.double(0.7)
 offlinePrimaryVertices1D=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVertices1D", particles="trackRefsForJetsBeforeSorting1D")
 offlinePrimaryVertices1DWithBS=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVertices1D:WithBS", particles="trackRefsForJetsBeforeSorting1D")
-unsortedOfflinePrimaryVerticesLegacy = offlinePrimaryVertices.clone()
-offlinePrimaryVerticesLegacy=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVerticesLegacy", particles="trackRefsForJetsBeforeSortingLegacy")
-offlinePrimaryVerticesLegacyWithBS=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVerticesLegacy:WithBS", particles="trackRefsForJetsBeforeSortingLegacy")
 DA2DParameters.TkDAClusParameters.verbose = cms.untracked.bool(False)
 trackWithVertexRefSelectorBeforeSorting1D = trackWithVertexRefSelector.clone(vertexTag="unsortedOfflinePrimaryVertices1D")
 trackWithVertexRefSelectorBeforeSorting1D.ptMax=9e99
 trackWithVertexRefSelectorBeforeSorting1D.ptErrorCut=9e99
 trackRefsForJetsBeforeSorting1D = trackRefsForJets.clone(src="trackWithVertexRefSelectorBeforeSorting1D")
-trackWithVertexRefSelectorBeforeSortingLegacy = trackWithVertexRefSelector.clone(vertexTag="unsortedOfflinePrimaryVerticesLegacy")
-trackWithVertexRefSelectorBeforeSortingLegacy.ptMax=9e99
-trackWithVertexRefSelectorBeforeSortingLegacy.ptErrorCut=9e99
-trackRefsForJetsBeforeSortingLegacy = trackRefsForJets.clone(src="trackWithVertexRefSelectorBeforeSortingLegacy")
 
 unsortedOfflinePrimaryVertices=offlinePrimaryVertices.clone()
 offlinePrimaryVertices=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVertices", particles="trackRefsForJetsBeforeSorting")
@@ -64,12 +56,7 @@ _phase2_tktiming_vertexreco = cms.Sequence( tpClusterProducer *
                                             trackWithVertexRefSelectorBeforeSorting1D *
                                             trackRefsForJetsBeforeSorting1D *
                                             offlinePrimaryVertices1D *
-                                            offlinePrimaryVertices1DWithBS *
-                                            unsortedOfflinePrimaryVerticesLegacy *
-                                            trackWithVertexRefSelectorBeforeSortingLegacy *
-                                            trackRefsForJetsBeforeSortingLegacy *
-                                            offlinePrimaryVerticesLegacy *
-                                            offlinePrimaryVerticesLegacyWithBS                                            
+                                            offlinePrimaryVertices1DWithBS                                         
                                             )
 
 from Configuration.Eras.Modifier_phase2_timing_cff import phase2_timing
@@ -79,7 +66,6 @@ phase2_timing.toModify(
     unsortedOfflinePrimaryVertices,
     verbose = cms.untracked.bool(False),
     TkClusParameters = DA2DParameters,
-    TkFilterParameters=dict(minPt=0.7),
     TrackTimesLabel = cms.InputTag("trackTimeValueMapProducer:generalTracksConfigurableFlatResolutionModel"),
     TrackTimeResosLabel = cms.InputTag("trackTimeValueMapProducer:generalTracksConfigurableFlatResolutionModelResolution"),
 )

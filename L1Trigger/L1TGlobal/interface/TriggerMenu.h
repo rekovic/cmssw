a95fb13a1b1ff05286106b5bfb76ff9d6296d151
Vladimir <vladimir.rekovic@cern.ch>
2017-04-05 21:02:32 +0200

    Dictionary creation for class CorrelationWithOverlapRemovalTemplate.

--- L1Trigger/L1TGlobal/interface/TriggerMenu.h
+++ L1Trigger/L1TGlobal/interface/TriggerMenu.h
@@ -34,6 +34,7 @@
 #include "L1Trigger/L1TGlobal/interface/EnergySumTemplate.h"
 #include "L1Trigger/L1TGlobal/interface/ExternalTemplate.h"
 #include "L1Trigger/L1TGlobal/interface/CorrelationTemplate.h"
+#include "L1Trigger/L1TGlobal/interface/CorrelationWithOverlapRemovalTemplate.h"
 
 
 // forward declarations

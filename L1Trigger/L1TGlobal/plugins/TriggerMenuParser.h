68809c4045b0704f1588bd7aaffd1d72bcdb0f25
Vladimir <vladimir.rekovic@cern.ch>
2021-04-28 12:55:04 +0200

    Revert "Merge branch 'cavana-upt-invariant-mass_l1t_11_2_0' into l1t-integration-CMSSW_11_2_0"
    This reverts commit 16475c21a2628e09c1d232daea92d15156f86b5c, reversing
    changes made to 2e7644fdbf4205eeef057193e055d17625218444.

--- L1Trigger/L1TGlobal/plugins/TriggerMenuParser.h
+++ L1Trigger/L1TGlobal/plugins/TriggerMenuParser.h
@@ -15,8 +15,6 @@
  * \author  Vladimir Rekovic
  *                - indexing
  *                - correlations with overlap object removal
- * \author R. Cavanaugh
- *                - displaced muons
  *
  * $Date$
  * $Revision$
@@ -295,12 +293,6 @@ namespace l1t {
                       std::string lutpfx,
                       std::string obj1,
                       unsigned int prec);
-
-    // Parse LUT for Upt LUT in Mass calculation for displaced muons
-    void parseUpt_LUTS(std::map<std::string, tmeventsetup::esScale> scaleMap,
-		       std::string lutpfx,
-		       std::string obj1,
-		       unsigned int prec);
 
     // Parse LUT for Delta Eta and Cosh
     void parseDeltaEta_Cosh_LUTS(std::map<std::string, tmeventsetup::esScale> scaleMap,
